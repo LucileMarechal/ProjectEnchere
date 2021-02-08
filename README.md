241ec321da9fe28a62c0bdcadbd53a51684f412f
patri <patri@192.168.1.15>
2021-02-08 11:28:19 +0100

    Création de la DAL

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ProjectEnchere
 Projet ENI
+test
---
+++ src/fr/eni/ecole/projectenchere/dal/TestDAOImpl.java
@@ -0,0 +1,9 @@
+package fr.eni.ecole.projectenchere.dal;
+
+public class TestDAOImpl {
+	
+	public void insert() {
+		System.out.println("init Test DAO");
+	}
+
+}
