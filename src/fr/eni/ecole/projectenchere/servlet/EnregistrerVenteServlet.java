b4f7753ad3a46dbbd2f0f62424492735280ee713
Administrateur <Administrateur@10SE5-2XH3P02>
2021-02-09 12:36:01 +0100

    mise à jour

--- src/fr/eni/ecole/projectenchere/servlet/EnregistrerVenteServlet.java
+++ src/fr/eni/ecole/projectenchere/servlet/EnregistrerVenteServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class EnregistrerVenteServlet
  */
-@WebServlet("/accueil.html")
+@WebServlet("/EnregistrerVente.html")
 public class EnregistrerVenteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
