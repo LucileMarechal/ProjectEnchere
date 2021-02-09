dda6efca91203a0bee22d06d5aab4ce8b5c98abe
Administrateur <Administrateur@10SE5-2XH3P02>
2021-02-09 11:30:38 +0100

    mise à jour

---
+++ src/fr/eni/ecole/projectenchere/servlet/AnnulerVenteEnCoursSerlvet.java
@@ -0,0 +1,35 @@
+package fr.eni.ecole.projectenchere.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AnnulerVenteEnCoursSerlvet
+ */
+@WebServlet("/AnnulerVenteEnCoursSerlvet")
+public class AnnulerVenteEnCoursSerlvet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AnnulerVenteEnCoursSerlvet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB/INF/jsp/accueilAvecConnexion.jsp").forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("je suis dans la doPost AnnulerVenteEnCoursServlet");
+	}
+
+}
