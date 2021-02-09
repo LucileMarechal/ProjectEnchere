188027cd549b14db2fd06f1486011ecc6ae90607
patri <patri@192.168.1.15>
2021-02-09 16:44:14 +0100

    Changement di nom de la WebServlet

--- src/fr/eni/ecole/projectenchere/servlet/accueilAvecConnexionServlet.java
+++ src/fr/eni/ecole/projectenchere/servlet/accueilAvecConnexionServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class accueilAvecConnexionServlet
  */
-@WebServlet("/accueilAvecConnexionServlet")
+@WebServlet("/accueilAvecConnexion.html")
 public class accueilAvecConnexionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
