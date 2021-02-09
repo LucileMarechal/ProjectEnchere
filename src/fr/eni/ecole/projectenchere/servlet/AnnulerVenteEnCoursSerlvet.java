956a892038153860d3f53dbb8e73c26e6096e222
Administrateur <Administrateur@10SE5-2XH3P02>
2021-02-09 12:35:08 +0100

    mise à jour

--- src/fr/eni/ecole/projectenchere/servlet/AnnulerVenteEnCoursSerlvet.java
+++ src/fr/eni/ecole/projectenchere/servlet/AnnulerVenteEnCoursSerlvet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AnnulerVenteEnCoursSerlvet
  */
-@WebServlet("/accueil.html")
+@WebServlet("/AnnulerVenteEnCours.html")
 public class AnnulerVenteEnCoursSerlvet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
