c4c7a29f2832bf47477827f4d3493c007ac240ac
Administrateur <Administrateur@10SE5-2XH3P02>
2021-02-09 12:38:19 +0100

    mise à jour

--- src/fr/eni/ecole/projectenchere/servlet/AnnulerVenteEnCoursSerlvet.java
+++ src/fr/eni/ecole/projectenchere/servlet/AnnulerVenteEnCoursSerlvet.java
@@ -24,7 +24,7 @@ public class AnnulerVenteEnCoursSerlvet extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB/INF/jsp/accueilAvecConnexion.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/jsp/accueilAvecConnexion.jsp").forward(request, response);
 	}
 
 	
