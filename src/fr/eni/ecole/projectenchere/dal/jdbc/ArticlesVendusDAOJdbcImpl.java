66ac2c83d26292a56cc6faad1e59e8b259c9682c
patri <patri@192.168.1.15>
2021-02-10 17:34:50 +0100

    mise à jour

--- src/fr/eni/ecole/projectenchere/dal/jdbc/ArticlesVendusDAOJdbcImpl.java
+++ src/fr/eni/ecole/projectenchere/dal/jdbc/ArticlesVendusDAOJdbcImpl.java
@@ -7,8 +7,12 @@ public class ArticlesVendusDAOJdbcImpl {
 	private static final String INSERT_ARTICLES_VENDUS = "INSERT INTO articles_vendus(nom_article, description, date_debut_enchere,"
 			+ "prix_initial, prix_vente, no_utilisateur, no_categorie) VALUES(?, ?, ?, ?, ?, ?, ?, ?);";
 	
+	private static final String SELECT_ARTICLES_VENDUS = "SELECT no_article, nom_article, date_debut_encheres, date_fin_encheres, "
+			+ "prix_initial, prix_vente,no_utilisateur, no_categorie, no_retrait FROM ARTICLES_VENDUS";
 	
+	private static final String UPDATE_ARTICLES_VENDUS ="UPDATE articles_vendus SET no_article=?, nom_article=?, date_debut_encheres=?, "
+			+ "date_fin_encheres=?, prix_initial=?, prix_vente,no_utilisateur=?, no_categorie=?, no_retrait=? WHERE no_article=?";
 	
-	
+	private static final String DELETE_ARTICLES_VENDUS ="DELETE FROM articles_vendus WHERE no_article=?";
 
 }
