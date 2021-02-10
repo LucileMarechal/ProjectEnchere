9d971b1036a537af32b7d0b95bba779c9804f5b6
Administrateur <Administrateur@10SE5-2XH3P02.home>
2021-02-10 15:38:24 +0100

    mise à jour

---
+++ src/fr/eni/ecole/projectenchere/dal/DBConnexion.java
@@ -0,0 +1,51 @@
+package fr.eni.ecole.projectenchere.dal;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DBConnexion {
+
+	public static Connection seConnecter() throws DALException {
+		Connection connection = null;
+		InitialContext jndi = null;
+		DataSource dsEnchere = null;
+		
+		try {
+			jndi = new InitialContext();
+		} catch (NamingException e) {
+			throw new DALException("Erreur d'accès au contexte initial JNDI", e);
+		}
+
+		try {
+			dsEnchere = (DataSource) jndi.lookup("java:comp/env/dsRepas");
+		} catch (NamingException e) {
+			throw new DALException("Objet est introuvable dans l'arbre JNDI", e);
+		}
+
+		//Obtention de la connexion à la BDD à partir de la dataSource
+
+		try {
+			connection = dsEnchere.getConnection();
+		} catch (SQLException e) {
+			throw new DALException("Impossible d'obtenir une connexion", e);
+		}
+
+		return connection;
+	}
+
+	public static void seDeconnecter (Connection connection) throws DALException {
+
+		try {
+			if(connection != null) {
+				connection.close();
+			}
+		} catch (SQLException e) {
+			throw new DALException("Impossible de fermer la connexion", e);
+		}
+		seDeconnecter(connection);
+	}
+}
--- src/fr/eni/ecole/projectenchere/dal/jdbc/ConnexionJdbcImpl.java
+++ src/fr/eni/ecole/projectenchere/dal/jdbc/ConnexionJdbcImpl.java
@@ -9,37 +9,6 @@ import com.microsoft.sqlserver.jdbc.SQLServerDriver;
 
 public class ConnexionJdbcImpl {
 
-	public static void main(String[] args) {
-		Connection connection = null;
-		Statement stmt = null;
-		
-		try {
-			//ETAPE 1 : chargement du pilote JDBC
-			DriverManager.registerDriver(new SQLServerDriver());
-			
-			//ETAPE 2 : Connection
-			String url = "jdbc:sqlserver://127.0.0.1;databasename=ENCHERES";
-			connection = DriverManager.getConnection(url, "sa", "Pa$$w0rd");
-			
-			//ETAPE 3 : Créer une requete avec le statement
-			stmt = connection.createStatement();
-			
-			//ETAPE 4 : execution de la requete
-			
-			
-			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-				try {
-					if (stmt!=null) {
-						stmt.close();
-					}
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+	
 	}
 
