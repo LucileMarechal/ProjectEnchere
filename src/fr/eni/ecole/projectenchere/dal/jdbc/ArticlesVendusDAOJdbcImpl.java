2507c9070b2af4268086e9ab0c5dd55b5de030f1
Panda <Panda@192.168.1.56>
2021-02-10 16:56:55 +0100

    dao articles vendus

--- src/fr/eni/ecole/projectenchere/dal/jdbc/ArticlesVendusDAOJdbcImpl.java
+++ src/fr/eni/ecole/projectenchere/dal/jdbc/ArticlesVendusDAOJdbcImpl.java
@@ -1,5 +1,14 @@
 package fr.eni.ecole.projectenchere.dal.jdbc;
 
 public class ArticlesVendusDAOJdbcImpl {
+	
+	// constantes
+	// INSERT ARTICLES VENDUS : no_article auto incrémenté : PK
+	private static final String INSERT_ARTICLES_VENDUS = "INSERT INTO articles_vendus(nom_article, description, date_debut_enchere,"
+			+ "prix_initial, prix_vente, no_utilisateur, no_categorie) VALUES(?, ?, ?, ?, ?, ?, ?, ?);";
+	
+	
+	
+	
 
 }
