49ab09d3099f1b6a8b3045e3f11d85f07c01eae8
Panda <Panda@192.168.1.56>
2021-02-10 15:35:54 +0100

    dal exception + suppression applitestbll

--- src/fr/eni/ecole/projectenchere/bll/AppliTestBLL.java
+++
@@ -1,5 +0,0 @@
-package fr.eni.ecole.projectenchere.bll;
-
-public class AppliTestBLL {
-
-}
---
+++ src/fr/eni/ecole/projectenchere/dal/DALException.java
@@ -0,0 +1,25 @@
+package fr.eni.ecole.projectenchere.dal;
+
+public class DALException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public DALException() {
+		super();
+	}
+
+	public DALException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DALException(String message) {
+		super(message);
+	}
+
+	@Override
+	public String getMessage() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("Couche DAL - " + super.getMessage());
+		return sb.toString();
+	}
+}
