4a2b7837ce69ee4de0f2f560b29a59828254dac4
Panda <Panda@192.168.1.56>
2021-02-10 11:57:15 +0100

    Ajout script table retraits fichier sql

---
+++ ScriptSQL/Insert table retraits.sql
@@ -0,0 +1,24 @@
+INSERT INTO [dbo].[RETRAITS]
+           ([rue]
+           ,[code_postal]
+           ,[ville])
+ VALUES
+ ('3 rue des coquelicots', '14200', 'Granville'),
+
+ ('5 boulevard Walt Disney','50400','Romainville'),
+
+ ('1 impasse de la grotte','44000','Nantes'),
+
+ ('3 rue du chateau','89000','Village des gorons'),
+
+('15 rue de babylone','13000','Marseille'),
+
+ ('26 boulevard des bicyles', '35000', 'Rennes'),
+
+ ('65 boulevard du café des amis','06400','New York'),
+
+ ('65 boulevard du café des amis','06400','New York'),
+
+ ('impasse du nid du dragon','54000','Denver'),
+
+('allée des Pokemon','14000','Pokeville');
