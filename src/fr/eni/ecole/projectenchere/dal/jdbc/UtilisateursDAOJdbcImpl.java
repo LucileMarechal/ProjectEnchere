79f40b3b4c4dc977e173f32517c4d8922a9cd33b
patri <patri@192.168.1.15>
2021-02-10 16:58:04 +0100

    Ajout utilisateursDAOJdbcImpl

---
+++ src/fr/eni/ecole/projectenchere/dal/jdbc/UtilisateursDAOJdbcImpl.java
@@ -0,0 +1,12 @@
+package fr.eni.ecole.projectenchere.dal.jdbc;
+
+public class UtilisateursDAOJdbcImpl {
+	
+	//Constantes
+	 private static final String INSERT_UTILISATEUR = "INSERT INTO utilisateurs (pseudo, nom, prenom, email, "
+	 		+ "telephone, rue, code_postal, vile, mot_de_passe, credit,administrateur) "
+	 		+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	
+	
+
+}
