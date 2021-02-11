8e5c6f866da25f3299c83f99e307e2e2e9391068
Panda <Panda@192.168.1.56>
2021-02-11 16:12:11 +0100

    testDALVince pour méthode selectAll utilisateurs

--- src/fr/eni/ecole/projectenchere/servlet/TestDALVince.java
+++ src/fr/eni/ecole/projectenchere/servlet/TestDALVince.java
@@ -1,23 +1,31 @@
 package fr.eni.ecole.projectenchere.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.ecole.projectenchere.bo.Utilisateur;
+import fr.eni.ecole.projectenchere.dal.DALException;
+import fr.eni.ecole.projectenchere.dal.DAOFactory;
+import fr.eni.ecole.projectenchere.dal.UtilisateursDAO;
+
 /**
  * Servlet implementation class TestDAL
  */
 @WebServlet("/TestDAL")
-public class TestDAL extends HttpServlet {
+public class TestDALVince extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public TestDAL() {
+    public TestDALVince() {
         super();
     }
 
@@ -25,6 +33,29 @@ public class TestDAL extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// tester de la méthode selectAll()
+		List<Utilisateur> utilisateurs = new ArrayList<>();
+		UtilisateursDAO utilisateursDAO = null;
+		String message = "";
+		
+		try {
+			utilisateursDAO = DAOFactory.getUtilisateursDAO();
+			utilisateurs = utilisateursDAO.selectAll();
+			if (utilisateurs.isEmpty()) {
+				message = "aucun utilisateur";
+				response.getWriter().append(message);
+			} else {
+				response.getWriter().append(utilisateurs.toString());
+			}
+			
+			
+		} catch (DALException e) {
+			response.getWriter().append(e.getMessage());
+		}
+		
+		
+		
+		
 		
 	}
 
