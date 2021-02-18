91b367bb71926cdacaf9066583f4974c1ca8406f
patri <patri@192.168.1.15>
2021-02-18 02:56:02 +0100

    Amélio jsp nouvelle vente

--- src/fr/eni/ecole/projectenchere/servlet/NouvelleVenteServlet.java
+++ src/fr/eni/ecole/projectenchere/servlet/NouvelleVenteServlet.java
@@ -2,6 +2,9 @@ package fr.eni.ecole.projectenchere.servlet;
 
 import java.io.IOException;
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import fr.eni.ecole.projectenchere.bll.ArticleVendusManager;
+import fr.eni.ecole.projectenchere.bll.CategoriesManager;
 import fr.eni.ecole.projectenchere.bo.ArticleVendu;
+import fr.eni.ecole.projectenchere.bo.Categories;
 import fr.eni.ecole.projectenchere.dal.DALException;
 
 
@@ -31,6 +36,23 @@ public class NouvelleVenteServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String pseudo = (String) request.getSession().getAttribute("loginSession");
 		System.out.println(pseudo);
+		List<Categories> categorie = new ArrayList<>();
+		CategoriesManager categories = new CategoriesManager();
+		String message="";
+		
+		try {
+			categorie = categories.CategorieManager().selectAll();
+			if (categorie.isEmpty()) {
+				message = "Pas de catégorie en BDD";
+				response.getWriter().append(message);
+			}else {
+				response.getWriter().append(categorie.toString());
+			}
+		} catch (DALException e) {
+			response.getWriter().append(e.getMessage());
+		} 
+				
+		request.setAttribute("listeCategorie", categorie);
 		
 		request.getRequestDispatcher("/WEB-INF/jsp/nouvelleVente.jsp").forward(request, response);
 	}
@@ -41,6 +63,9 @@ public class NouvelleVenteServlet extends HttpServlet {
 		ArticleVendu articleAVendre = new ArticleVendu();
 		ArticleVendusManager artA = new ArticleVendusManager();
 		
+		List<Categories> categorie = new ArrayList<>();
+		CategoriesManager categories = new CategoriesManager();
+		String message="";
 		
 		String nomArticle = request.getParameter("sarticle");
 		String description = request.getParameter("sdescription");
@@ -80,11 +105,22 @@ public class NouvelleVenteServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 		
+		try {
+			categorie = categories.CategorieManager().selectAll();
+			if (categorie.isEmpty()) {
+				message = "Pas de catégorie en BDD";
+				response.getWriter().append(message);
+			}else {
+				response.getWriter().append(categorie.toString());
+			}
+		} catch (DALException e) {
+			response.getWriter().append(e.getMessage());
+		} 
+				
+		request.setAttribute("listeCategorie", categorie);
+		
 		
 		}
-	
-	
-	
-	
+
 	}
 
