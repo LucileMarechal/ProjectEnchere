5063a03cc0890603504fc63a09483fe2d2471238
patri <patri@192.168.1.15>
2021-02-19 08:45:31 +0100

    mise à jour script

--- ScriptSQL/Articles_vendus/Jointure ArticlesVendus et Utilisateurs + recherche par mot clé.sql
+++ ScriptSQL/Articles_vendus/Jointure ArticlesVendus et Utilisateurs + recherche par mot clé.sql
@@ -1,4 +1,16 @@
-SELECT nom_article, prix_initial, date_fin_encheres, pseudo
+
+select GETDATE()
+
+
+SET DATEFORMAT dmy;
+SELECT nom_article, prix_initial, date_fin_encheres, pseudo, ARTICLES_VENDUS.no_categorie
 			FROM ARTICLES_VENDUS
 			INNER JOIN UTILISATEURS ON  ARTICLES_VENDUS.no_utilisateur = UTILISATEURS.no_utilisateur
-			where nom_article like 'ROBE';
+			INNER JOIN CATEGORIES ON CATEGORIES.no_categorie = ARTICLES_VENDUS.no_categorie
+			WHERE libelle like 'Informatique' and date_fin_encheres > GETDATE()
+
+SELECT nom_article, prix_initial, date_fin_encheres, pseudo, ARTICLES_VENDUS.no_categorie, UTILISATEURS.no_utilisateur
+			FROM ARTICLES_VENDUS
+			INNER JOIN UTILISATEURS ON  ARTICLES_VENDUS.no_utilisateur = UTILISATEURS.no_utilisateur
+			WHERE date_fin_encheres > getdate()
+
